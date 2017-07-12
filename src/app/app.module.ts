d5e6fafdcf50e013a42ea07b3cfb438246172b9a
maschad <chad.nehemiah94@gmail.com>
2017-07-12 08:50:10 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserSelectionComponent } from './user-selection/user-selection.component';
+import { ThreadSectionComponent } from './thread-section/thread-section.component';
+import { MessageSectionComponent } from './message-section/message-section.component';
+import { MessageListComponent } from './message-list/message-list.component';
+import { ThreadListComponent } from './thread-list/thread-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSelectionComponent,
+    ThreadSectionComponent,
+    MessageSectionComponent,
+    MessageListComponent,
+    ThreadListComponent
   ],
   imports: [
     BrowserModule

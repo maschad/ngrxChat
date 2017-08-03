a03249ffd6271a53809dae027da975bde497a9ea
maschad <chad.nehemiah94@gmail.com>
2017-08-03 00:16:30 -0500

    Integrated ngrx/effects [forgot to add folder in commit]

---
+++ src/app/store/effects/load-threads-effect.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {ThreadsService} from "../../services/threads.service";
+import {Effect, Actions} from "@ngrx/effects";
+import {LOAD_USER_THREADS_ACTION, UserThreadsLoadedAction} from "../actions";
+import {Observable} from "rxjs";
+import {Action} from "@ngrx/store";
+
+
+@Injectable()
+export class LoadThreadsEffectService {
+
+  constructor(private actions$: Actions, private threadsService:ThreadsService) { }
+
+    @Effect() userThreads$: Observable<Action> = this.actions$
+      .ofType(LOAD_USER_THREADS_ACTION)
+      .switchMap(() => this.threadsService.loadUserThreads())
+      .map(allUserData => new UserThreadsLoadedAction(allUserData));
+
+}
